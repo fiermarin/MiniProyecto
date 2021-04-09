53f1655cd44ff7bd4fedcc015e86cbf0124ddf2d
Ramiro Soca <Ramiro Soca@LAPTOP-O4QO8JC7>
2021-04-09 13:12:58 -0300

    gim

---
+++ MiniProyecto/src/MiniProyecto/Gimnasio.java
@@ -0,0 +1,27 @@
+package MiniProyecto;
+
+public class Gimnasio {
+			private String espacio;
+
+		public Gimnasio(String espacio) {
+			
+			this.espacio = espacio;
+		}
+
+		public Gimnasio() {
+			
+		}
+
+		public String getEspacio() {
+			return espacio;
+		}
+
+		public void setEspacio(String espacio) {
+			this.espacio = espacio;
+		}
+		
+		
+		
+		
+	}
+
