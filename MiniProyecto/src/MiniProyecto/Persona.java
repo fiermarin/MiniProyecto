2cab8834788c97fc45c355ae16cab9b87870cd80
Matia <Matia@DESKTOP-8BCP2PD>
2021-04-09 12:53:10 -0300

    a

---
+++ MiniProyecto/src/MiniProyecto/Persona.java
@@ -0,0 +1,93 @@
+package MiniProyecto;
+
+public class Persona {
+	private String nombre;
+	private String apellido;
+	private int ci;
+	private int telefono;
+	private String direccion;
+	private String fechaNacimiento;
+	private boolean carnetSalud;
+	private String sexo;
+	
+	public Persona() {
+		super();
+	}
+
+	public Persona(String nombre, String apellido, int ci, int telefono, String direccion, String fechaNacimiento,
+			boolean carnetSalud, String sexo) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.ci = ci;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.fechaNacimiento = fechaNacimiento;
+		this.carnetSalud = carnetSalud;
+		this.sexo = sexo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getCi() {
+		return ci;
+	}
+
+	public void setCi(int ci) {
+		this.ci = ci;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public boolean isCarnetSalud() {
+		return carnetSalud;
+	}
+
+	public void setCarnetSalud(boolean carnetSalud) {
+		this.carnetSalud = carnetSalud;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}
